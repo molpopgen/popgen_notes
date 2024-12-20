8ffec844cda196b704f9d19026f14c17b621422d
Kevin R. Thornton <krthornt@uci.edu>
2024-12-20 07:53:18 -0800

    First pass at pedigree text (#5)
    * various fixes to scripts to match R package expectations

--- install_r_packages.sh
+++ install_r_packages.sh
@@ -1,6 +1,6 @@
-packages=(kinship2)
+packages=(kinship2 pedtools)
 
-for p in ${packages}
+for p in ${packages[@]}
 do
     R --no-save --quiet -e "install.packages('$p', repos='http://cran.us.r-project.org')"
 done
